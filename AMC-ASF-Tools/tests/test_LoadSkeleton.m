752f862e7bde88ff164fb6022fe41f7f160c51b3
Morgan <zyigo3000@gmail.com>
2020-09-05 15:52:17 +0930

    update file path in test

--- AMC-ASF-Tools/tests/test_LoadSkeleton.m
+++ AMC-ASF-Tools/tests/test_LoadSkeleton.m
@@ -13,7 +13,7 @@ initTestSuite;
 function test_LoadSkeleton_0
 % test if loaded skeleton matches expected data
 %f_skel = LoadSkeletion('data/02.asf');
-saveVarsMat = load('data/matlab.mat');
+saveVarsMat = load('tests/data/matlab.mat');
 test_skel = saveVarsMat.ans; 
 disp(test_skel);
 %assertEqual(test_skel,f_skel);
